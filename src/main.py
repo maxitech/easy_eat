6bc269b4e8432ab9471b0996e278c3fc2b603684
maxitech <derstadlmax@gmail.com>
2024-08-18 14:43:11 +0200

    feature: implemented search and select function

--- src/main.py
+++ src/main.py
@@ -26,7 +26,35 @@ values_list = worksheet.get_all_records()
 df = pd.DataFrame(values_list)
 
 
+st.title('Easy Eat')
 
+st.subheader('Rezeptvorschau')
+st.write(df.head())
+
+# filter sheet
+search_input = st.text_input('Suche ein Rezept:', help='Suchparameter: Gericht | Kategorie | Ernährungsweise | Dauer | Zutaten').strip()
+
+def search_recipes(df, search_param): 
+      return df[df.apply(lambda row: row.astype(str).str.contains(search_param, case=False, na=False).any(), axis=1)]
+
+if search_input:
+    filtered_df = search_recipes(df, search_input)
+    
+    if not filtered_df.empty:
+        st.subheader('Gefundene Rezepte:')
+        st.write(filtered_df)
+    else:
+        st.subheader('Es wurde kein Gericht gefunden.')
+      
+        
+st.subheader('Optional:')
+columns = df.columns.tolist()
+selected_column = st.selectbox('Wähle eine Spalte nach der gefiltert werden soll:', columns)
+unique_values = df[selected_column].unique()
+selected_value = st.selectbox('Wähle einen Wert:', unique_values)
+
+filtered_df = df[df[selected_column] == selected_value]
+st.write(filtered_df)
 
 
 
